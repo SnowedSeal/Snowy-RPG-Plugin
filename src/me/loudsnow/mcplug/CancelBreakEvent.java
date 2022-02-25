825c03d85212340f0ab6145a56557057ea951c6d
Loudbook <loudbooked@gmail.com>
2022-02-25 18:39:27 -0500

    Fixed Cancel Break Event

--- src/me/loudsnow/mcplug/CancelBreakEvent.java
+++ src/me/loudsnow/mcplug/CancelBreakEvent.java
@@ -1,12 +1,23 @@
 package me.loudsnow.mcplug;
 
-/*
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public class CancelBreakEvent implements Listener {
     @EventHandler
-    public void onBlockBreak(Play)
+    public void onBlockBreak(BlockBreakEvent event){
+        Player player = event.getPlayer();
+        if (player.getDisplayName().equals("RGolden")) {
+        }
+        else if (player.getDisplayName().equals("Loudbook")) {
+        }
+        else if (player.getDisplayName().equals("FabledLivid")) {
+        }
+        else {
+            event.setCancelled(true);
+
+        }
+    }
 }
-*/
