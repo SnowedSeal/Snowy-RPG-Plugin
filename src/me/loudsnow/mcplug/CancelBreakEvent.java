67452c3ddf5863dc532a3393a1162f5f8290b835
Loudbook <loudbooked@gmail.com>
2022-02-25 16:07:57 -0500

    Implemented Block Break Restrictions (Not running)

---
+++ src/me/loudsnow/mcplug/CancelBreakEvent.java
@@ -0,0 +1,12 @@
+package me.loudsnow.mcplug;
+
+/*
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class CancelBreakEvent implements Listener {
+    @EventHandler
+    public void onBlockBreak(Play)
+}
+*/
