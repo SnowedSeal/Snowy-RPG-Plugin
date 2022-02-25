d9ea5404d1bf5a4e23a2d2b80fae1661c659a077
Loudbook <loudbooked@gmail.com>
2022-02-25 14:05:25 -0500

    Cancel Portal Creation

---
+++ src/me/loudsnow/mcplug/CancelPortal.java
@@ -0,0 +1,15 @@
+package me.loudsnow.mcplug;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.PortalCreateEvent;
+
+public class CancelPortal implements Listener {
+    @EventHandler
+    public void onPortalCreate(PortalCreateEvent event){
+        event.setCancelled(true);
+
+
+    }
+}
