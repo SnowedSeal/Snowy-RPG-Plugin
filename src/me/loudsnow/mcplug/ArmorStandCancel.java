3ba2b9e379fcac36c69930436dd870cbcab7119d
Loudbook <loudbooked@gmail.com>
2022-02-27 17:36:55 -0500

    Update ArmorStandCancel.java

--- src/me/loudsnow/mcplug/ArmorStandCancel.java
+++ src/me/loudsnow/mcplug/ArmorStandCancel.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
-
+/*
 public class ArmorStandCancel implements Listener {
     @EventHandler
     public void onStandManipulation(PlayerArmorStandManipulateEvent event) {
@@ -17,3 +17,4 @@ public class ArmorStandCancel implements Listener {
         }
     }
 }
+*/
