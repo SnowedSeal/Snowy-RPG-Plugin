57f492ad21e8a5dd9caa4a7fd7770ea73e335924
Loudbook <loudbooked@gmail.com>
2022-03-04 11:14:51 -0500

    Create build.gradle.kts

---
+++ .github/workflows/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+}
+
+// The main package name
+group = "shattered.software"
+// Using semver instead of SNAPSHOT versioning
+version = "1.0.0"
+
+// Places we can get libraries
+repositories {
+    // Use local versions first
+    mavenLocal()
+    // Look in Spigot's repos next
+    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    // Look in Maven Central last
+    mavenCentral()
+}
+
+// Libraries we need
+dependencies {
+    // We want Spigot
+    implementation("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
+}
