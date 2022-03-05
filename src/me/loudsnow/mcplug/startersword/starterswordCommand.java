0ec3ae7dce8eb835864aefca0059c6fb73304fd1
Loudbook <loudbooked@gmail.com>
2022-03-05 14:36:48 -0500

    Update Main (#5)
    * 2 starter items + Desolation fixes / additions (#3)
    
    * Add files via upload
    
    * did an oopsy
    
    * Desolation Item
    
    * another oopsy
    
    * Desolation Item
    
    * Added actual code
    
    * +Potion effects/Enchant glint
    
    Sweeping edge 3 for large sweep attack, mining fatigue 2 and slowness 2 for balancing reasons.
    
    * Moved a few things
    
    Temporarily removed potion effects, currently migrating to Listener.
    
    * Added desolation ability + 2 starter items
    
    also started work on a Awakened Desolation
    
    * Removed an extra line
    
    * name change
    
    * added commands
    
    * added color to the name
    
    * added commands
    
    * needed to change something
    
    * Added back
    
    * Delete plugin.yml
    
    * Delete plugin.yml
    
    Co-authored-by: Loudbook <loudbooked@gmail.com>
    
    * Update Beta Branch (#4)
    
    * Create maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Create README.md
    
    * Create codeql-analysis.yml
    
    * Create workspace.xml
    
    * Update README.md
    
    * Delete Loud and Snow's SMP.iml
    
    Co-authored-by: FabledLivid <62721547+Shadowgod2008@users.noreply.github.com>

---
+++ src/me/loudsnow/mcplug/startersword/starterswordCommand.java
@@ -0,0 +1,43 @@
+package me.loudsnow.mcplug.startersword;
+
+
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.EquipmentSlot;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class starterswordCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+
+            Player p = (Player) sender;
+            ItemStack basicsword = new ItemStack(Material.IRON_SWORD, 1);
+            ItemMeta meta = basicsword.getItemMeta();
+            meta.setDisplayName(ChatColor.GRAY + "Basic Sword");
+            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(),  "generic.attackDamage", 8, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
+            meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            meta.isUnbreakable();
+            if(p.getItemInHand() != null && p.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.GRAY + "Basic Sword");) {
+                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, new AttributeModifier(UUID.randomUUID(),  "generic.attackSpeed", 1.8, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
+            }
+            List<String> lore = new ArrayList<>();
+            lore.add("It's a solid starter weapon!");
+            meta.setLore(lore);
+            p.getInventory().addItem(basicsword);
+            return true;
+
+        }
+        return false;
+    }
+}
