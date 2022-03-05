b0f8b2db5f81904a2cd29fcb720d04ffe366a48a
Loudbook <loudbooked@gmail.com>
2022-03-05 15:22:33 -0500

    Added training dummy that show damage

--- src/main/java/me/loudsnow/mcplug/TrainingDummy.java
+++ src/main/java/me/loudsnow/mcplug/TrainingDummy.java
@@ -4,13 +4,14 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Damageable;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.World;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+
+import java.util.List;
 
 import static me.loudsnow.mcplug.Main.*;
 
@@ -19,42 +20,39 @@ public class TrainingDummy implements Listener {
     Boolean used = false;
     @EventHandler
     public void onDamage(EntityDamageEvent e) {
+        Entity entity = e.getEntity();
+        String uuid = entity.getUniqueId().toString();
+        if (uuid.equals("fc00e200-5434-2ebc-9754-db5e9e10c8e2")) {
+            List<Entity> nearby = entity.getNearbyEntities(1, 1, 1);
+            for (Entity tmp : nearby)
+                if (tmp instanceof Damageable) {
+                    if (tmp instanceof Player) {
 
+                    } else {
+                        tmp.remove();
 
-        if (boolea.containsKey("Yes")) {
-            e.setCancelled(true);
-
-
-
-            } else {
-                Entity entity = e.getEntity();
-                String uuid = entity.getUniqueId().toString();
-                if (uuid.equals("50fd99dc-628f-274d-b86d-5c8dc3ad6a98")) {
-
-                    ((Damageable) entity).damage(0.1);
-                    ((Damageable) entity).setHealth(20);
-                    Bukkit.broadcastMessage("Test");
-
-                    e.setCancelled(true);
-
-                    Double damage = e.getDamage();
-                    ((Damageable) entity).damage(0.1);
-                    ((Damageable) entity).setHealth(20);
-                    Entity damaged = e.getEntity();
-
-                    damaged.setCustomNameVisible(true);
-                    damaged.setCustomName(damage + " damage!");
-                    boolea.put("Yes", 0);
-
-                    Bukkit.getScheduler().scheduleSyncRepeatingTask(instance, new Runnable() {
-                        @Override
-                        public void run() {
-                            damaged.setCustomName("");
-                            boolea.remove("Yes");
-
-                        }
-                    }, 100, -1);
+                    }
                 }
+            e.setCancelled(true);
+            World world = entity.getWorld();
+            ArmorStand armorstand = (ArmorStand) world.spawnEntity(entity.getLocation(), EntityType.ARMOR_STAND);
+            armorstand.setVisible(false);
+
+
+
+            Double damage = e.getDamage();
+            Entity damaged = e.getEntity();
+            int damagerounded = (int) Math.round(damage);
+
+            armorstand.setCustomNameVisible(true);
+            armorstand.setCustomName("" + ChatColor.RED + ChatColor.BOLD + damagerounded +  " damage!");
+            Bukkit.getScheduler().runTaskLater(instance, new Runnable() {
+                @Override
+                public void run() {
+                    armorstand.remove();
+
+                }
+            }, 50);
         }
     }
 }
