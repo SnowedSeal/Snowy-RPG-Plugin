48c2fe2a5c2d27d9ddaba03de0f408f527e517e8
FabledLivid <62721547+Shadowgod2008@users.noreply.github.com>
2022-03-06 17:14:19 -0500

    Salvation and other stuff idk (#9)
    * Add files via upload
    
    * did an oopsy
    
    * Desolation Item
    
    * another oopsy
    
    * Desolation Item
    
    * Added actual code
    
    * +Potion effects/Enchant glint
    
    Sweeping edge 3 for large sweep attack, mining fatigue 2 and slowness 2 for balancing reasons.
    
    * Moved a few things
    
    Temporarily removed potion effects, currently migrating to Listener.
    
    * Added desolation ability + 2 starter items
    
    also started work on a Awakened Desolation
    
    * Removed an extra line
    
    * name change
    
    * added commands
    
    * added color to the name
    
    * added commands
    
    * needed to change something
    
    * Added back
    
    * Delete plugin.yml
    
    * Delete plugin.yml
    
    * added commands here ig
    
    * missed the import
    
    * forgor another one
    
    * Delete Main.java
    
    * Salvation item no ability yet
    
    Co-authored-by: Loudbook <loudbooked@gmail.com>

---
+++ src/main/java/me/loudsnow/mcplug/salvation/salvationCommand.java
@@ -0,0 +1,45 @@
+package me.loudsnow.mcplug.salvation;
+
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.EquipmentSlot;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class salvationCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+
+            Player p = (Player) sender;
+            ItemStack salvation = new ItemStack(Material.GOLD_INGOT, 1);
+            ItemMeta meta = salvation.getItemMeta();
+            meta.setDisplayName("" + ChatColor.GOLD +"Salvation");
+            if(p.getEquipment().getItemInOffHand() != null && p.getEquipment().getItemInOffHand().getItemMeta().getDisplayName().equals("" + ChatColor.GOLD +"Salvation")) {
+                meta.addAttributeModifier(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "generic.armorValue", 2, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.OFF_HAND));
+                meta.addAttributeModifier(Attribute.GENERIC_MAX_HEALTH, new AttributeModifier(UUID.randomUUID(), "generic.healthValue", 5, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.OFF_HAND));
+            }
+            meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            meta.isUnbreakable();
+            List<String> lore = new ArrayList<>();
+            lore.add("" + ChatColor.GOLD + ChatColor.BOLD +"A chalice forged by the angels.It is said");
+            lore.add("" + ChatColor.GOLD + ChatColor.BOLD +"to have incredible healing powers.");
+            lore.add("" + ChatColor.YELLOW +"Use in offhand it gain buffs!");
+            meta.setLore(lore);
+            p.getInventory().addItem(salvation);
+            return true;
+        }
+        return false;
+    }
+}
