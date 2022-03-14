d5f1ba043e89877e0b9d5568f0e9256e62787f7a
Loudbook <loudbooked@gmail.com>
2022-03-13 21:23:17 -0400

    Make it so you cant take off the saddle on a horse

---
+++ src/main/java/me/loudsnow/mcplug/cancelevents/CancelSaddle.java
@@ -0,0 +1,25 @@
+package me.loudsnow.mcplug.cancelevents;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.HorseInventory;
+
+public class CancelSaddle implements Listener {
+    @EventHandler
+    public void onInventoryInteract(InventoryClickEvent e){
+        Player p = (Player) e.getWhoClicked();
+        if (e.getInventory() instanceof HorseInventory) {
+
+            if (e.getCurrentItem().equals(Material.SADDLE)){
+                e.setCancelled(true);
+
+
+            }
+        }
+    }
+}
+// getTitle().equalsIgnoreCase("" + ChatColor.GOLD + ChatColor.BOLD + p.getName().toString() + ChatColor.YELLOW + ChatColor.BOLD + "'s horse")
