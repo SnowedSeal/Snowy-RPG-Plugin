ec3d3ea078b7691297818836b788748d7cbebb8b
Loudbook <loudbooked@gmail.com>
2022-03-13 21:26:58 -0400

    More MongoDB stuff

---
+++ src/main/java/me/loudsnow/mcplug/QuickStart.java
@@ -0,0 +1,21 @@
+package me.loudsnow.mcplug;
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class QuickStart {
+
+    public static void main( String[] args ) {
+        // Replace the uri string with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://Loudbook:W%40S45282@cluster0.phfoz.mongodb.net/cluster0?retryWrites=true&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            System.out.println(doc.toJson());
+        }
+    }
+}
