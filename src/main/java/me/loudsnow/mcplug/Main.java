c2b6f9cb73ccfd9fc7ed0dd5f9e503e78ba2cce6
Loudbook <loudbooked@gmail.com>
2022-03-13 21:25:43 -0400

    Join stuff

--- src/main/java/me/loudsnow/mcplug/Main.java
+++ src/main/java/me/loudsnow/mcplug/Main.java
@@ -4,9 +4,17 @@ import me.loudsnow.mcplug.cancelevents.*;
 import me.loudsnow.mcplug.desolation.DesolationAbilityListener;
 import me.loudsnow.mcplug.desolation.DesolationCommand;
 import me.loudsnow.mcplug.desolation.DesolationAttackListener;
+import me.loudsnow.mcplug.flameblade.FlamebladeCommand;
+import me.loudsnow.mcplug.flameblade.FlamebladeListener;
+import me.loudsnow.mcplug.forestsword.ForestSwordCommand;
+import me.loudsnow.mcplug.glowingpendant.GlowingPendantCommand;
 import me.loudsnow.mcplug.meteorslam.MeteorSlamCommand;
 import me.loudsnow.mcplug.meteorslam.MeteorSlamListener;
 import me.loudsnow.mcplug.npcs.*;
+import me.loudsnow.mcplug.rookieglider.RookieGliderCommand;
+import me.loudsnow.mcplug.rookieglider.RookieGliderListener;
+import me.loudsnow.mcplug.salvation.SalvationCommand;
+import me.loudsnow.mcplug.salvation.SalvationListener;
 import me.loudsnow.mcplug.shadowleap.ShadowleapCommand;
 import me.loudsnow.mcplug.shadowleap.ShadowleapListener;
 import me.loudsnow.mcplug.starterdagger.starterdaggerCommand;
@@ -15,12 +23,22 @@ import me.loudsnow.mcplug.truedeso.trueDesoCommand;
 import me.loudsnow.mcplug.truedeso.trueDesoListener;
 import me.loudsnow.mcplug.windstep.WindstepCommand;
 import me.loudsnow.mcplug.windstep.WindstepListener;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.HorseInventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class Main extends JavaPlugin {
@@ -36,9 +54,14 @@ public class Main extends JavaPlugin {
     public static HashMap<String, Integer> cd4 = new HashMap<>();
     public static HashMap<String, Integer> cd5 = new HashMap<>();
     public static HashMap<String, Integer> cd6 = new HashMap<>();
+    public static HashMap<String, Integer> cd7 = new HashMap<>();
+    public static HashMap<String, Integer> cd8 = new HashMap<>();
+    public static HashMap<String, Integer> horse = new HashMap<>();
+    public static HashMap<String, Integer> horsealive = new HashMap<>();
+    public static HashMap<UUID, UUID> drop = new HashMap<>();
     public static boolean desolationdmg = false;
-
-
+    public static Map<String, String> player = new HashMap<String, String>();
+    public static Map<String, String> horseowner = new HashMap<String, String>();
 
 
     @Override
@@ -59,7 +82,7 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new NPC6(), this);
         this.getCommand("getwindstep").setExecutor(new WindstepCommand());
         getServer().getPluginManager().registerEvents(new WindstepListener(), this);
-        getServer().getPluginManager().registerEvents(new CancelDropEvent(), this);
+        getServer().getPluginManager().registerEvents(new CancelItemPickup(), this); // <- this
         getServer().getPluginManager().registerEvents(new CancelFallDamage(), this);
         this.getCommand("getmeteorslam").setExecutor(new MeteorSlamCommand());
         this.getCommand("getshadowleap").setExecutor(new ShadowleapCommand());
@@ -67,123 +90,138 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new MeteorSlamListener(), this);
         getServer().getPluginManager().registerEvents(new CancelBedEnter(), this);
         getServer().getPluginManager().registerEvents(new ShadowleapListener(), this);
-        getServer().getPluginManager().registerEvents(new TrainingDummy(), this);
         this.getCommand("getdesolation").setExecutor(new DesolationCommand());
         getServer().getPluginManager().registerEvents(new DesolationAttackListener(), this);
         getServer().getPluginManager().registerEvents(new DesolationAbilityListener(), this);
         this.getCommand("gettruedesolation").setExecutor(new trueDesoCommand());
         this.getCommand("getstarterdagger").setExecutor(new starterdaggerCommand());
         this.getCommand("getstartersword").setExecutor(new starterswordCommand());
+        this.getCommand("getrookieglider").setExecutor(new RookieGliderCommand());
         getServer().getPluginManager().registerEvents(new trueDesoListener(), this);
+        getServer().getPluginManager().registerEvents(new TrainingDummy(), this);
+        getServer().getPluginManager().registerEvents(new CancelHorseRide(), this);
+        getServer().getPluginManager().registerEvents(new RookieGliderListener(), this);
+        this.getCommand("getflameblade").setExecutor(new FlamebladeCommand());
+        getServer().getPluginManager().registerEvents(new FlamebladeListener(), this);
+        getServer().getPluginManager().registerEvents(new SalvationListener(), this);
+        getServer().getPluginManager().registerEvents(new SalvationListener(), this);
+        getServer().getPluginManager().registerEvents(new NPC8(), this);
+        getServer().getPluginManager().registerEvents(new HorseListener(), this);
+        this.getCommand("getsalvation").setExecutor(new SalvationCommand());
+        this.getCommand("getforestsword").setExecutor(new ForestSwordCommand());
+        this.getCommand("getglowingpendant").setExecutor(new GlowingPendantCommand());
+        this.getCommand("hashmaptest").setExecutor(new TestHashCommand());
+        this.getCommand("whistle").setExecutor(new Whistle());
+        getServer().getPluginManager().registerEvents(new HorseDeath(), this);
+        getServer().getPluginManager().registerEvents(new QuitEvent(), this);
+        //getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+        getServer().getPluginManager().registerEvents(new CancelPVP(), this);
+        getServer().getPluginManager().registerEvents(new CancelHorseDamage(), this);
+        getServer().getPluginManager().registerEvents(new CancelSaddle(), this);
+
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("data.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String key : properties.stringPropertyNames()) {
+            player.put(key, properties.get(key).toString());
+        }
+
+
+        Properties properties1 = new Properties();
+        try {
+            properties1.load(new FileInputStream("horse.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String key : properties1.stringPropertyNames()) {
+            horseowner.put(key, properties1.get(key).toString());
+        }
 
 
 
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+            public void run(){
+                for (Player p : Bukkit.getOnlinePlayers()){
+                    ItemStack power = new ItemStack(Material.BLAZE_POWDER);
+                    ItemMeta meta = power.getItemMeta();
+                    meta.setDisplayName("" + ChatColor.BLUE + ChatColor.BOLD + "Glowing Pendant");
+                    power.setItemMeta(meta);
 
+                    if (p.getInventory().containsAtLeast(power, 1))  {
+                        p.sendMessage("You have the item!");
+                    }
+                }
+            }
+        }, 20L, 10L);
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+            public void run(){
+                for(World w : Bukkit.getWorlds()) {
+                    for (Entity entity : w.getEntities()) {
+                        if (entity instanceof Horse) {
+                            if (((Horse) entity).getOwner() != null) {
+                                Player owner = (Player) ((Horse) entity).getOwner();
+                                ((Horse) entity).setTarget(owner);
+                            }
+                        }
+                    }
+                }
+
+            }
+        }, 0L, 5L);
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+            public void run(){
+
+            }
+        }, 0L, 5L);
 
 
         // HashMaps.bank = loadHashMap("bank");
 
         ConsoleCommandSender console = getServer().getConsoleSender();
-        System.out.println(bank.get("Loudbook"));
 
 
     }
 
     @Override
     public void onDisable() {
-        // saveHashMap(HashMaps.bank, ("bank"));
+        Properties properties = new Properties();
+        Properties properties1 = new Properties();
+        ConsoleCommandSender console = getServer().getConsoleSender();
+        console.sendMessage("Saving Hashmap");
 
+
+        for (Map.Entry<String, String> entry : player.entrySet()) {
+            properties.put(entry.getKey(), entry.getValue());
         }
 
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    public void restoreBank() {
-        this.getConfig().getConfigurationSection("data").getKeys(false).forEach(key ->{
-            @SuppressWarnings("unchecked")
-            Integer[] amount = ((List<Integer>) this.getConfig().get("data." + key)).toArray(new Integer[0]);
-            bank.put(key, amount);
-        });
-
-    }
-
-
-}
-
-
-    public static void saveHashMap(HashMap<String, Integer> hm, String hashName) {
-        file = new File(Main.instance.getDataFolder(), hashName + ".yml");
-        System.out.println(Main.instance.getDataFolder().toString());
-
-        if (!file.exists()){
-            try {
-                file.createNewFile();
-                System.out.println("3");
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("4");
-            }
-        }
-
-
-        YamlConfiguration customFile = YamlConfiguration.loadConfiguration(file);
-        // For every KEY the HashMap has...
-        for (Object key : hm.keySet()) {
-            // Use the YamlConfiguration to be able to read the new file as a .yml
-            // and set it as needed, so that each player has their own file.
-            // Each "player" file will have all of their stats such as XP
-            // MaxHP, their level, coins, etc.
-            customFile.set(hashName + "."+key, hm.get(key));
-
-        }
         try {
-            customFile.save(file);
+            properties.store(new FileOutputStream("data.properties"), null);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-    }
 
-
-
-    public static HashMap<String, Integer> loadHashMap(String hashName) {
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
-
-        file = new File(Main.instance.getDataFolder(), hashName + ".yml");
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-                System.out.println("3");
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("4");
-            }
+        for (Map.Entry<String, String> entry : horseowner.entrySet()) {
+            properties1.put(entry.getKey(), entry.getValue());
         }
 
-        YamlConfiguration customFile = YamlConfiguration.loadConfiguration(file);
-        for (String key : customFile.getConfigurationSection(hashName).getKeys(true)) {
-            // Load the config from the file, and apply it to the hashmaps needed.
-            hm.put(key, (int) customFile.get(hashName + "." + key));
-        }
-        return hm;
-    }
+        try {
+            properties1.store(new FileOutputStream("horse.properties"), null);
+        } catch (IOException e) {
+            e.printStackTrace();
 
+        }
+    }
 }
-*/
+
+
+
+
+
+
+
